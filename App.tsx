0e98288b735b1f6553f1a4f503a92a7440c1e147
Martin Masek <masek.ma@gmail.com>
2021-03-15 19:00:21 +0100

    added permissions

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { usePermissions } from "expo-permissions";
+import * as Permissions from "expo-permissions";
+import React from "react";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [permission, askForPermission] = usePermissions(Permissions.CAMERA, {
+    ask: true,
+  });
+
+  if (!permission || permission.status !== "granted") {
+    return (
+      <SafeAreaView>
+        <Text>Permission is not granted</Text>
+        <Button title="Grant permission" onPress={askForPermission} />
+      </SafeAreaView>
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -12,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
